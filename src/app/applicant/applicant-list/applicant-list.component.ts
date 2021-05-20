a9453e15df2f673165b508d64efa19aaf06ffb77
Xavier <xaavier@protonmail.com>
2021-05-20 21:44:48 +0200

    Done applicant-list component. TODO: Solve api problem on posting/creating an applicant since status 400, gets blank DNI

--- src/app/applicant/applicant-list/applicant-list.component.ts
+++ src/app/applicant/applicant-list/applicant-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Applicant } from 'src/app/models/applicant';
+import { ApplicantService } from '../applicant.service';
 
 @Component({
   selector: 'app-applicant-list',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicantListComponent implements OnInit {
 
-  constructor() { }
+  public applicants: Applicant[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalApplicants = 0;
+  
+  constructor(private router: Router,
+              private applicantService: ApplicantService) { }
 
   ngOnInit(): void {
+    this.applicantService.getAll({size: this.pageSize}).subscribe(
+      (applicants: Applicant[]) => {
+        this.applicants = applicants;
+        this.totalApplicants = this.applicantService.totalElement();
+      }
+    );
+  }
+
+  changePage(): void {
+    this.applicantService.page(this.page -1).subscribe(
+      (applicants: Applicant[]) => this.applicants = applicants
+    );
+  }
+
+  getCurrentApplicant(): Applicant{
+    return this.applicantService.getCurrentApplicant();
   }
 
 }
