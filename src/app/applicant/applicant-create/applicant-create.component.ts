72abf0ec5918407613e6dce3acee24a74fcfe3d0
Xavier <xaavier@protonmail.com>
2021-05-20 21:30:59 +0200

    Done service and create component

--- src/app/applicant/applicant-create/applicant-create.component.ts
+++ src/app/applicant/applicant-create/applicant-create.component.ts
@@ -1,4 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationBasicService } from 'src/app/login-basic/authentication-basic.service';
+import { Applicant } from 'src/app/models/applicant';
+import { ApplicantService } from '../applicant.service';
 
 @Component({
   selector: 'app-applicant-create',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicantCreateComponent implements OnInit {
 
-  constructor() { }
+  public applicant: Applicant;
+
+  constructor(private router: Router,
+              private location: Location,
+              private applicantService: ApplicantService,
+              private authenticationBasicService: AuthenticationBasicService) { }
 
   ngOnInit(): void {
+    this.applicant = new Applicant();
   }
 
+  onSubmit(): void {
+    this.applicantService.create(this.applicant).subscribe(
+      (newApplicant: Applicant) => {
+        this.router.navigate(['applicant']);
+      }
+    );
+  }
+
+  onCancel(): void {
+    this.location.back();
+  }
 }
--- src/app/applicant/applicant.service.ts
+++ src/app/applicant/applicant.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { RestService } from '@lagoshny/ngx-hal-client';
+import { Applicant } from '../models/applicant';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApplicantService {
+export class ApplicantService extends RestService<Applicant> {
 
-  constructor() { }
+  constructor(injector: Injector) {
+    super(Applicant, 'applicants', injector);
+  }
+
+  getCurrentApplicant(): Applicant {
+    return new Applicant(JSON.parse(localStorage.getItem('currentApplicant')));
+  }
 }
