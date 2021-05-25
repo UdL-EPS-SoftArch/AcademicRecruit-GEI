65368598b61507621b7e0d459261a96f836acf17
Roberto García <roberto@rhizomik.net>
2021-05-25 15:41:10 +0200

    Applicant dni instead of DNI to match API

--- src/app/models/applicant.ts
+++ src/app/models/applicant.ts
@@ -3,10 +3,11 @@ import { Resource} from '@lagoshny/ngx-hal-client';
 
 export class Applicant extends Resource{
   id: string;
-  email: string
+  email: string;
   name: string;
   lastname: string;
-  DNI: string;
+  dni: string;
+
   constructor(values: object = {}) {
     super();
     Object.assign(this as any, values);
