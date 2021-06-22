c3839ff8689badd20b34e0d4068f6b72aaed8124
Xavier <xaavier@protonmail.com>
2021-06-22 13:06:59 +0200

    Done phase components stuff

--- src/app/phase/phase-delete/phase-delete.component.ts
+++ src/app/phase/phase-delete/phase-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Phase } from 'src/app/models/phase';
+import { PhaseService } from '../phase.service';
 
 @Component({
   selector: 'app-phase-delete',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhaseDeleteComponent implements OnInit {
 
-  constructor() { }
+  public phase: Phase = new Phase();
+  private id: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private phaseService: PhaseService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.phaseService.get(this.id).subscribe(
+      phase => this.phase = phase
+    );
+  }
+
+  delete(): void {
+    this.phaseService.delete(this.phase).subscribe(
+      () => {
+        this.router.navigate(['']);
+      }
+    );
   }
 
 }
